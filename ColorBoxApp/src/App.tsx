2b0095c134fef89174a086e24ab9dd1c10e4904f
Payal <payalbute6@gmail.com>
2026-07-05 21:56:31 +0530

    Commit

---
+++ ColorBoxApp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BoxContainer from './components/BoxContainer'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+ return(
+  <div className= "App">
+    <BoxContainer/>
+  </div>
+ );
+}
+
+
+export default App
